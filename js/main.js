85e0ccee35d3cac5b76c07e0343215c3d6307869
Khadija Aydinli <khadijaaydinligmail.com@Khadijas-MacBook-Air.local>
2024-08-08 14:02:24 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// person
+// car 
+// school 
+// sport 
+// hobby
+
+person = {
+    Name: "Mady",
+    Surname: "Albertson",
+    Age: 16,
+    Gender: "Female",
+    Height: "1.72",
+    eyeColor: "black"
+}
+console.log(person)
+
+car = {
+    Brand: "Tesla",
+    Model: "Cybertruck",
+    Production_year: "2023",
+    Color: "silver"
+}
+console.log(car)
+
+school = {
+    Name: "Argonaut High School",
+    NumberOfStudents: "550",
+    Grade: 9-12,
+    Class: 11
+}
+console.log(school)
+
+sport = {
+    Type: "basketball",
+    IsEngaged: "4 years",
+    TeamName: "Fires"
+}
+console.log(sport)
+
+hobby = {
+    name: "drawing",
+    StartEngageIn: 10
+}
+console.log(hobby)
+
